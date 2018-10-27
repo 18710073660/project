668581ad97754017de4d7faf0d1c24b38e727b9e
gt1234 <823183651@qq.com>
2018-10-27 22:28:36 +0800

    edit

--- src/main/java/com/gt/DemoApplication.java
+++ src/main/java/com/gt/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.gt.mapper")
+@MapperScan("com.gt.web.mapper")
 public class DemoApplication {
 
 	public static void main(String[] args) {
