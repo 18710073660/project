42cceaeab40d7f40e4a8cc962dcea60261351bdf
gt1234 <823183651@qq.com>
2018-10-28 21:58:00 +0800

    【add】销量模块

--- src/main/java/com/gt/web/controller/ProductController.java
+++ src/main/java/com/gt/web/controller/ProductController.java
@@ -1,5 +1,7 @@
 package com.gt.web.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -8,11 +10,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.google.gson.Gson;
+import com.gt.web.entity.Echarts;
 import com.gt.web.entity.Product;
+import com.gt.web.entity.Series;
 import com.gt.web.entity.TableData;
+import com.gt.web.entity.User;
 import com.gt.web.service.ProductService;
 
 @Controller
@@ -40,6 +46,42 @@ public class ProductController<E> {
         String str = gson.toJson(td);
         logger.info("str:"+str);
         return td;
+    }
+    
+    @RequestMapping("/toMyEcharts")
+    public String toMyEcharts(Model model,@RequestParam("param") String param) {
+    	model.addAttribute("param", param);
+    	return "productEcharts";
+    }
+    
+    @RequestMapping("/prepareDataForEcharts")
+    @ResponseBody
+    public Echarts prepareDataForEcharts(Model model,@RequestParam("param") String param) {
+    	List<String> list = Arrays.asList(param.split(","));
+    	List<Product> productList = new ArrayList<Product>();
+    	for (String str : list) {
+    		productList.add(productService.getProductById(str));
+		}
+    	
+    	
+    	List<String> legend = new ArrayList<String>();
+    	legend.add("销量折线图");
+    	
+    	List<String> axisList = new ArrayList<String>();
+    	List<Integer> series = new ArrayList<Integer>();
+    	for (Product product : productList) {
+    		axisList.add(product.getName());
+			series.add(product.getNumber());
+		}
+    	
+        List<Series> seriesList = new ArrayList<Series>();
+        seriesList.add(new Series("销量", "line", new ArrayList<Integer>(series)));
+        Echarts echarts = new Echarts(legend, axisList, seriesList);
+        Gson gson = new Gson();
+        String str = gson.toJson(echarts);
+        logger.info("str:"+str);
+        
+    	return echarts;
     }
     
 }
